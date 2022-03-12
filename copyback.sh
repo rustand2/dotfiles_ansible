0bb6df372112623299f706cff9311a01b90573af
Lars Rustand <rustand.lars@gmail.com>
2022-03-12 23:03:20 +0100

    Add template diffing to copyback script

--- copyback.sh
+++ copyback.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-FILES=$(find roles -wholename '*/files/*' -type f)
 
-for F in $FILES; do
-    LOCAL_FILE=$(echo $F | sed -e 's/^.*files//g' | sed -e "s|HOME|home/$USER|g")
-    cp $LOCAL_FILE $F
+for FILE in $(find roles -wholename '*/files/*' -type f); do
+    LOCAL_FILE=$(echo $FILE | sed -e 's/^.*files//g' | sed -e "s|HOME|home/$USER|g")
+    cp $LOCAL_FILE $FILE
+done
+
+mkdir -p temp
+for TEMPLATE in $(find roles -wholename '*/templates/*' -type f); do
+    LOCAL_FILE=$(echo -n "$TEMPLATE" | sed -e 's/^.*templates//g' | sed -e "s|HOME|home/$USER|g" | sed -e "s/\.j2$//g")
+    FILE=$(basename "$LOCAL_FILE")
+    ansible all -i "localhost," -m template -a "src=$TEMPLATE dest=temp/$FILE" --connection=local -e @vars.yml &> /dev/null
+    diff -q $LOCAL_FILE temp/$FILE
+    rm temp/$FILE
 done
