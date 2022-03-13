aa4a4a2d3f7adfb4a8ac96fa299593291d2940fa
Lars Rustand <rustand.lars@gmail.com>
2022-03-13 13:20:37 +0100

    Fix command not found

--- roles/zsh/files/etc/zsh/command-not-found.zsh
+++ roles/zsh/files/etc/zsh/command-not-found.zsh
@@ -1,20 +1,22 @@
 command_not_found_handler() {
   local pkgs cmd="$1"
 
-  pkgs=(${(f)"$(pkgfile -b -v -- "$cmd" 2>/dev/null)"})
-  if [[ -n "$pkgs" ]]; then
-    printf '%s may be found in the following packages:\n' "$cmd"
-    printf '  %s\n' $pkgs[@]
-	echo -n "Replace? "
-	while true; do
-		read yn
-		case $yn in
-			[Yy]* ) echo "OK!"; break;;
-			[Nn]* ) echo "Nop!"; break;;
-			* ) echo "Please answer yes or no.";;
-		esac
-	done
-    return 0
+  if [[ -x "$(which pkgfile)" ]]; then
+    pkgs=(${(f)"$(pkgfile -b -v -- "$cmd" 2>/dev/null)"})
+    if [[ -n "$pkgs" ]]; then
+      printf '%s may be found in the following packages:\n' "$cmd"
+      printf '  %s\n' $pkgs[@]
+      echo -n "\nInstall it now? "
+      while true; do
+          read yn
+          case $yn in
+              [Yy]* ) sudo pacman -S "$(echo "$pkgs" | cut -d' ' -f1)"; break;;
+              [Nn]* ) ; break;;
+              * ) echo "Please answer yes or no.";;
+          esac
+      done
+      return 0
+    fi
   fi
 
   printf 'zsh: command not found: %s\n' "$cmd" 1>&2
