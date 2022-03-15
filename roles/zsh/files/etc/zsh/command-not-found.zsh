102b07534324f729f3993c6f65e152bb9106b307
Lars Rustand <rustand.lars@gmail.com>
2022-03-15 11:36:54 +0100

    Silence pkgfile output in command-not-found

--- roles/zsh/files/etc/zsh/command-not-found.zsh
+++ roles/zsh/files/etc/zsh/command-not-found.zsh
@@ -1,7 +1,7 @@
 command_not_found_handler() {
   local pkgs cmd="$1"
 
-  if which pkgfile; then
+  if which pkgfile &> /dev/null; then
     pkgs=(${(f)"$(pkgfile -b -v -- "$cmd" 2>/dev/null)"})
     if [[ -n "$pkgs" ]]; then
       printf '%s may be found in the following packages:\n' "$cmd"
