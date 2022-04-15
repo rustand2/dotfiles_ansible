c23e2e2b341d9ed03ccc05f67c5f4ddf4ba3d0a8
Lars Rustand <rustand.lars@gmail.com>
2022-04-15 21:08:07 +0200

    Support password store organized in subfolders

--- roles/mail/offlineimap/files/HOME/.offlineimap/auth.py
+++ roles/mail/offlineimap/files/HOME/.offlineimap/auth.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python
 import subprocess
+import os
 
+def get_account_path(account):
+    password_store = f"{os.path.expanduser('~')}/.password-store"
+    args = ["find", password_store, "-wholename", f"*/mutt/{account}/client_id.gpg"]
+    sub = subprocess.run(args, check=True, capture_output=True)
+    filepath = "/".join(sub.stdout.decode("utf-8").split("/")[4:-1])
+    return filepath
 
 def get_client_id(account):
-    args = ["/usr/bin/pass", "show", f"mutt/{account}/client_id"]
+    path = get_account_path(account)
+    args = ["/usr/bin/pass", "show", f"{path}/client_id"]
     sub = subprocess.run(args, check=True, capture_output=True)
     return sub.stdout.decode("utf-8")
 
 
 def get_client_secret(account):
-    args = ["/usr/bin/pass", "show", f"mutt/{account}/client_secret"]
+    path = get_account_path(account)
+    args = ["/usr/bin/pass", "show", f"{path}/client_secret"]
     sub = subprocess.run(args, check=True, capture_output=True)
     return sub.stdout.decode("utf-8")
 
 
 def get_refresh_token(account):
-    args = ["/usr/bin/pass", "show", f"mutt/{account}/refresh_token"]
+    path = get_account_path(account)
+    args = ["/usr/bin/pass", "show", f"{path}/refresh_token"]
     sub = subprocess.run(args, check=True, capture_output=True)
     return sub.stdout.decode("utf-8")
