1cc985827d8fde79633277c2d58d10d9ee33fff7
Lars Rustand <rustand.lars@gmail.com>
2022-04-16 01:47:55 +0200

    Add basic outlook/office365 imap support

--- roles/mail/offlineimap/files/HOME/.offlineimap/auth.py
+++ roles/mail/offlineimap/files/HOME/.offlineimap/auth.py
@@ -2,15 +2,38 @@
 import subprocess
 import os
 
+
 def get_account_path(account):
     password_store = f"{os.path.expanduser('~')}/.password-store"
-    args = ["find", password_store, "-wholename", f"*/mutt/{account}/app_password.gpg"]
+    args = ["find", password_store, "-wholename", f"*/mutt/{account}"]
     sub = subprocess.run(args, check=True, capture_output=True)
-    filepath = "/".join(sub.stdout.decode("utf-8").split("/")[4:-1])
-    return filepath
+    filepath = "/".join(sub.stdout.decode("utf-8").split("/")[4:])
+    return filepath.replace("\n","")
+
 
 def get_app_password(account):
     path = get_account_path(account)
     args = ["/usr/bin/pass", "show", f"{path}/app_password"]
+    sub = subprocess.run(args, check=True, capture_output=True)
+    return sub.stdout.decode("utf-8")
+
+
+def get_client_id(account):
+    path = get_account_path(account)
+    args = ["/usr/bin/pass", "show", f"{path}/client_id"]
+    sub = subprocess.run(args, check=True, capture_output=True)
+    return sub.stdout.decode("utf-8")
+
+
+def get_client_secret(account):
+    path = get_account_path(account)
+    args = ["/usr/bin/pass", "show", f"{path}/client_secret"]
+    sub = subprocess.run(args, check=True, capture_output=True)
+    return sub.stdout.decode("utf-8")
+
+
+def get_refresh_token(account):
+    path = get_account_path(account)
+    args = ["/usr/bin/pass", "show", f"{path}/refresh_token"]
     sub = subprocess.run(args, check=True, capture_output=True)
     return sub.stdout.decode("utf-8")
