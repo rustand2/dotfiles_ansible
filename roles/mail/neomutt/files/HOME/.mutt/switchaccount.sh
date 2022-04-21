8d10ee5190f4901358423f0adbf496147fc79e71
Lars Rustand <rustand.lars@gmail.com>
2022-04-21 17:28:35 +0200

    Move mutt config to .config/neomutt

--- roles/mail/neomutt/files/HOME/.mutt/switchaccount.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-ACCOUNT=$(find ~/.mutt/accounts -maxdepth 1 -mindepth 1 | grep -o '[^/]*$' | grep -v '\.' | fzf --border --reverse)
-echo "source ~/.mutt/accounts/$ACCOUNT" > ~/.mutt/curr_account
